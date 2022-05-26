4a57c9da9e27f7ac3db83fe061e9b3282f29d8f7
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-26 12:41:02 +0800

    feat: multiple models (solo added)

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -11,28 +11,37 @@ import sys
 
 from utils.model_downloader import download
 
-def init():
+def init(model_choice):
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
 
-    # Choose to use a config and initialize the detector
-    config = 'configs/scnet/scnet_r50_fpn_1x_coco.py'
+    if model_choice == 'd-solo-light':
+        config = 'configs/solo/decoupled_solo_light_r50_fpn_3x_coco.py'
+        checkpoint = 'checkpoints/'\
+        'decoupled_solo_light_r50_fpn_3x_coco_20210906_142703-e70e226f.pth'
+        url = "https://download.openmmlab.com/mmdetection/v2.0/solo/decoupled_solo_light_r50_fpn_3x_coco/decoupled_solo_light_r50_fpn_3x_coco_20210906_142703-e70e226f.pth"
+        logging.info("Using Decoupled Solo Light model.")
+    elif model_choice == 'scnet-r50-fpn':
+        config = 'configs/scnet/scnet_r50_fpn_1x_coco.py'
+        checkpoint = 'checkpoints/'\
+        'scnet_r50_fpn_1x_coco-c3f09857.pth'
+        url = "https://download.openmmlab.com/mmdetection/v2.0/scnet/scnet_r50_fpn_1x_coco/scnet_r50_fpn_1x_coco-c3f09857.pth"
+        logging.info("Using SCNet R-50-FPN model.")
+    else:
+        raise ValueError("Model choice not found. Choose from 'd-solo-light', 'scnet-r50-fpn'.")
     
     if not os.path.isfile(config):
         raise FileNotFoundError(
             errno.ENOENT, os.strerror(errno.ENOENT), config
         )
-
-    # Setup a checkpoint file to load
-    checkpoint = 'checkpoints/'\
-        'scnet_r50_fpn_1x_coco-c3f09857.pth'
         
     if not os.path.isfile(checkpoint):
         logging.warning("Checkpoint not found. Will attempt to download from MMDetection.")
-        download("https://download.openmmlab.com/mmdetection/v2.0/scnet/scnet_r50_fpn_1x_coco/scnet_r50_fpn_1x_coco-c3f09857.pth", dest_folder="checkpoints")
+        download(url, dest_folder="checkpoints")
 
     # Set the device to be used for evaluation
     device = "cuda:0" if is_available() else "cpu"
